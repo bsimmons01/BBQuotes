92c42159b30711fd2925895c6ba9c1a40765a19d
Brian Simmons <bsimmons01@gmail.com>
2025-03-05 13:22:51 -0700

    Init From Decoder

--- BBQuotes/Char.swift
+++ BBQuotes/Char.swift
@@ -22,4 +22,31 @@ struct Char: Decodable {
     // if let newProperty = myName {...}
     // similar:
     // guard let newProperty = myName {...}
+    
+    enum CodingKeys: CodingKey {
+        case name
+        case birthday
+        case occupations
+        case image
+        case aliases
+        case status
+        case portrayedBy
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.birthday = try container.decode(String.self, forKey: .birthday)
+        self.occupations = try container.decode([String].self, forKey: .occupations)
+        self.image = try container.decode([URL].self, forKey: .image)
+        self.aliases = try container.decode([String].self, forKey: .aliases)
+        self.status = try container.decode(String.self, forKey: .status)
+        self.portrayedBy = try container.decode(String.self, forKey: .portrayedBy)
+        
+        let deathDecoder = JSONDecoder()
+        deathDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let deathData = try Data(contentsOf: Bundle.main.url(forResource: "sampledeath", withExtension: "json")!)
+        death = try deathDecoder.decode(Death.self, from: deathData)
+    }
 }
