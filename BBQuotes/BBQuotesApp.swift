ec1a8493df72760b9867a0b63232ae7ffdf7da92
Brian Simmons <bsimmons01@gmail.com>
2025-03-05 08:44:58 -0700

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Brian Simmons on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
