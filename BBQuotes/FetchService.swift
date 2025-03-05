437f49edf2e153415e7bd3e295d94dacd355e014
Brian Simmons <bsimmons01@gmail.com>
2025-03-05 11:29:25 -0700

    Fetch service complete

--- BBQuotes/FetchService.swift
+++ BBQuotes/FetchService.swift
@@ -35,5 +35,52 @@ struct FetchService {
         return quote
     }
     
+    func fetchCharacter(_ name: String) async throws -> Char {
+        // Build fetch URL
+        let characterURL = baseURL.appending(path: "characters")
+        let fetchURL = characterURL.appending(queryItems: [URLQueryItem(name: "production", value: name)])
+        
+        // Fetch data - Built in Networking function
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        // Handle response
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        // Decode data
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let characters = try decoder.decode([Char].self, from: data)
+        
+        // Return characters
+        return characters[0]
+    }
     
+    func fetchDeath(for character: String) async throws -> Death? {
+        let fetchURL = baseURL.appending(path: "deaths")
+        
+        // Fetch data - Built in Networking function
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        // Handle response
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        // Decode data
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let deaths = try decoder.decode([Death].self, from: data)
+        
+        for death in deaths {
+            if death.character == character {
+                return death
+            }
+        }
+        
+        return nil
+    }
 }
